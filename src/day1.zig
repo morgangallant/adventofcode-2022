b94f7c6592ea04cbb0b5bab70c1db0b3a47d6caf
Morgan Gallant <morgan@morgangallant.com>
2022-12-04 14:20:53 +0900

    oh theres part 2, finished day1 and day2
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- src/day1.zig
+++ src/day1.zig
@@ -6,21 +6,35 @@ pub fn day1() !void {
 
     var buf: [1024]u8 = undefined;
     var current_sum: i32 = 0;
-    var max_sum: i32 = 0;
+    var sums = std.ArrayList(i32).init(std.heap.page_allocator);
     while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         // if line is empty, compare current_sum to max_sum and reset current_sum
         if (line.len == 0) {
-            if (current_sum > max_sum) {
-                max_sum = current_sum;
-            }
+            try sums.append(current_sum);
             current_sum = 0;
             continue;
         } else if (std.mem.eql(u8, line, "exit")) {
+            if (current_sum > 0) {
+                try sums.append(current_sum);
+            }
             break;
         }
         const num = try std.fmt.parseInt(i32, line, 10);
         current_sum += num;
     }
+    var x = sums.toOwnedSlice();
+    std.sort.sort(i32, x, {}, cmpByValue);
 
-    try stdout.print("{}\n", .{max_sum});
+    var i: usize = 0;
+    for (x) |val| {
+        if (i >= 3) {
+            break;
+        }
+        try stdout.print("{d}\n", .{val});
+        i += 1;
+    }
+}
+
+fn cmpByValue(context: void, a: i32, b: i32) bool {
+    return std.sort.desc(i32)(context, a, b);
 }
--- src/day2.zig
+++ src/day2.zig
@@ -4,30 +4,55 @@ pub fn day2() !void {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
 
+    const part2 = true;
+
     var buf: [4]u8 = undefined;
     var score: u32 = 0;
     while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         if (line[0] == 'e') break;
+        if (part2) {
+            line[2] = switch (line[2]) {
+                'X' => switch (line[0]) {
+                    'A' => 'Z',
+                    'B' => 'X',
+                    'C' => 'Y',
+                    else => unreachable,
+                },
+                'Y' => switch (line[0]) {
+                    'A' => 'X',
+                    'B' => 'Y',
+                    'C' => 'Z',
+                    else => unreachable,
+                },
+                'Z' => switch (line[0]) {
+                    'A' => 'Y',
+                    'B' => 'Z',
+                    'C' => 'X',
+                    else => unreachable,
+                },
+                else => unreachable,
+            };
+        }
         score += switch (line[0]) {
             'A' => switch (line[2]) {
                 'X' => 3 + 1,
                 'Y' => 6 + 2,
                 'Z' => 0 + 3,
-                else => 0,
+                else => unreachable,
             },
             'B' => switch (line[2]) {
                 'X' => 0 + 1,
                 'Y' => 3 + 2,
                 'Z' => 6 + 3,
-                else => 0,
+                else => unreachable,
             },
             'C' => switch (line[2]) {
                 'X' => 6 + 1,
                 'Y' => 0 + 2,
                 'Z' => 3 + 3,
-                else => 0,
+                else => unreachable,
             },
-            else => 0,
+            else => unreachable,
         };
     }
     try stdout.print("{}\n", .{score});
